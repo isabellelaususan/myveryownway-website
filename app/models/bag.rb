0eccb3ce1404d703070718386b5c6af8167d9443
Kimberly Agustin van der Leeuw <kimberlyavdl@gmail.com>
2024-02-18 17:07:25 +1100

    models completed

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -1,2 +1,5 @@
 class Bag < ApplicationRecord
+  belongs_to :product
+  has_many :types
+  has_many :colors
 end
--- app/models/pouch.rb
+++ app/models/pouch.rb
@@ -1,2 +1,5 @@
 class Pouch < ApplicationRecord
+  belongs_to :product
+  has_many :types
+  has_many :colors
 end
